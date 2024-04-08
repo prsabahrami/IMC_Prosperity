c2a335dd662b6a47e89dd438ed216d96ff4d1259
Alireza <mohaghegh.ar82@gmail.com>
2024-04-07 23:41:31 -0400

    Second commit!

--- trader.py
+++ trader.py
@@ -23,7 +23,7 @@ class Trader:
         print("traderData: " + state.traderData)
         print("Observations: " + str(state.observations))
         acc_prices = {'AMETHYSTS': 10000, 'STARFRUIT': 10}
-        coef = [4.997108e+03,  1.002812e-01, -6.605642e-04,  1.359603e-06, -1.241079e-09,  4.907114e-13, -5.258202e-17, -7.529245e-21]
+        coef = [4.999041e+03,  5.830593e-02, -4.869977e-04,  5.829545e-07,  5.793575e-09, -3.037098e-11,  6.805529e-14, -8.510963e-17,  6.322860e-20, -2.772803e-23, 6.634538e-27, -6.683339e-31]
 		# Orders to be placed on exchange matching engine
         result = {}
         for product in state.order_depths:
@@ -38,23 +38,32 @@ class Trader:
                     self.counts[product].pop(0)
                 print("Prices: " + str(self.prices[product]))
                 print("Counts: " + str(self.counts[product]))
+            for price in order_depth.sell_orders.keys():
+                quantity = order_depth.sell_orders[price]
+                self.prices[product].append(price)
+                self.counts[product].append(-1*quantity)
+                if len(self.prices[product]) > 10:
+                    self.prices[product].pop(0)
+                    self.counts[product].pop(0)
+                print("Prices: " + str(self.prices[product]))
+                print("Counts: " + str(self.counts[product]))
             if len(self.prices[product]) == 10 and product == 'STARFRUIT':
                 sma = self.ma(self.prices[product], self.counts[product])
                 t = state.timestamp / 100
-                acc_prices[product] = coef[0] + coef[1] * t + coef[2] * t ** 2 + coef[3] * t ** 3 + coef[4] * t ** 4 + coef[5] * t ** 5 + coef[6] * t ** 6 + coef[7] * t ** 7
+                acc_prices[product] = coef[0] + coef[1] * t + coef[2] * t ** 2 + coef[3] * t ** 3 + coef[4] * t ** 4 + coef[5] * t ** 5 + coef[6] * t ** 6 + coef[7] * t ** 7 + coef[8] * t ** 8 + coef[9] * t ** 9 + coef[10] * t ** 10 + coef[11] * t ** 11
                 acc_prices[product] = (acc_prices[product] + sma) / 2
             acceptable_price = acc_prices[product] # Participant should calculate this value
             print("Acceptable price : " + str(acceptable_price))
             print("Buy Order depth : " + str(len(order_depth.buy_orders)) + ", Sell order depth : " + str(len(order_depth.sell_orders)))
     
             if len(order_depth.sell_orders) != 0:
-                best_ask, best_ask_amount = list(order_depth.sell_orders.items())[0]
+                best_ask, best_ask_amount = list(sorted(order_depth.sell_orders.items(), reverse=True))[0]
                 if int(best_ask) < acceptable_price:
                     print("BUY", str(-best_ask_amount) + "x", best_ask)
                     orders.append(Order(product, best_ask, -best_ask_amount))
     
             if len(order_depth.buy_orders) != 0:
-                best_bid, best_bid_amount = list(order_depth.buy_orders.items())[0]
+                best_bid, best_bid_amount = list(sorted(order_depth.buy_orders.items()))[0]
                 if int(best_bid) > acceptable_price:
                     print("SELL", str(best_bid_amount) + "x", best_bid)
                     orders.append(Order(product, best_bid, -best_bid_amount))
--- tutorial/test.py
+++ tutorial/test.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('/home/alireza/Desktop/IMC_Prosperity/tutorial/data.csv', sep=';', dtype={'timestamp': int, 'mid_price': float})
+# df = pd.read_csv('/home/alireza/Desktop/IMC_Prosperity/tutorial/data.csv', sep=';', dtype={'timestamp': int, 'mid_price': float})
 
 
-filter = (df['product'] == 'AMETHYSTS')
+# filter = (df['product'] == 'AMETHYSTS')
 
-plt.plot(df[filter]['timestamp'], df[filter]['mid_price'])
-plt.show()
+# plt.plot(df[filter]['timestamp'], df[filter]['mid_price'])
+# plt.show()
 
+print(sorted({2:3, 1:5}.items()))
